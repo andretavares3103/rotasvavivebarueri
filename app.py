b2e02c165e35f40e64a69fa3a74ab430957a045b
andretavares3103 <andre.mtavares1@gmail.com>
2026-03-16 13:42:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,23 +23,23 @@ PORTAL_OS_LIST = "portal_atendimentos_os_list.json" # ou o nome correto da lista
 
 
 #########TESTE GOOGLE SHEETS
-    def conectar_google_sheet():
+def conectar_google_sheet():
 
-    scope = [
-        "https://spreadsheets.google.com/feeds",
-        "https://www.googleapis.com/auth/drive"
-    ]
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
 
-    creds = Credentials.from_service_account_file(
-        "credenciais_google.json",
-        scopes=scope
-    )
+creds = Credentials.from_service_account_file(
+    "credenciais_google.json",
+    scopes=scope
+)
 
-    client = gspread.authorize(creds)
+client = gspread.authorize(creds)
 
-    sheet = client.open("Aceites Rotas Vavive").sheet1
+sheet = client.open("Aceites Rotas Vavive").sheet1
 
-    return sheet
+return sheet
 
 
 
