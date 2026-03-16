3a0bed486450e65e2d009218f8c48d4aa672da83
andretavares3103 <andre.mtavares1@gmail.com>
2026-03-16 15:14:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def conectar_google_sheet():
     
     client = gspread.authorize(creds)
     
-    sheet = client.open("Aceites Rotas Vavive").sheet1
+    sheet = client.open_by_key("1O7ceSXeLsVi71n1T_-TMJWJGqL_URYbk92-groPSKUE")..worksheet("Aceites")
     
     return sheet
 
